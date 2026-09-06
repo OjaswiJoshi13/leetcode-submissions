ce2bdd189757da27e972e5b06b16836803bcbb2f
Ojaswi Joshi <ojaswijoshi581@gmail.com>
2026-09-06 19:23:38 +0530

    Time: 0 ms (100%), Space: 14.7 MB (5.67%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,33 +1,47 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& board, int r,int c,int m, int n, vector<vector<int>>& vis){
-        vector<int> dr={1,0,-1,0};
-        vector<int> dc={0,1,0,-1};
-
-        vis[r][c]=1;
-
-        for(int i=0;i<4;i++){
-            int nr=r+dr[i];
-            int nc=c+dc[i];
-
-            if(nr>=0 && nc>=0 && nr<m && nc<n && board[nr][nc]=='X' && !vis[nr][nc]){
-                vis[nr][nc]=1;
-                dfs(board,nr,nc,m,n,vis);
-            }
-        }
-    }
     int countBattleships(vector<vector<char>>& board) {
-        int m=board.size();
-        int n=board[0].size();
+        int m = board.size();
+        int n = board[0].size();
 
-        vector<vector<int>> vis(m,vector<int> (n));
+        vector<vector<int>> vis(m, vector<int>(n, 0));
 
-        int count=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]=='X' && !vis[i][j]){
+        int count = 0;
+
+        vector<int> dr = {1, 0, -1, 0};
+        vector<int> dc = {0, 1, 0, -1};
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+
+                // Start BFS only from an unvisited battleship cell
+                if (board[r][c] == 'X' && vis[r][c] == 0) {
+
                     count++;
-                    dfs(board,i,j,m,n,vis);
+
+                    queue<pair<int, int>> q;
+                    q.push({r, c});
+                    vis[r][c] = 1;
+
+                    while (!q.empty()) {
+                        int cr = q.front().first;
+                        int cc = q.front().second;
+                        q.pop();
+
+                        for (int i = 0; i < 4; i++) {
+                            int nr = cr + dr[i];
+                            int nc = cc + dc[i];
+
+                            if (nr >= 0 && nc >= 0 &&
+                                nr < m && nc < n &&
+                                board[nr][nc] == 'X' &&
+                                vis[nr][nc] == 0) {
+
+                                vis[nr][nc] = 1;
+                                q.push({nr, nc});
+                            }
+                        }
+                    }
                 }
             }
         }
